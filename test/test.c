70f0a85a67990b1dd99c0c49e3c11ba71d2242b9
Anya Orlova <anya-orlova-00@list.ru>
2020-10-19 09:49:22 +0700

    Updated test

--- test/test.c
+++ test/test.c
@@ -1,36 +1,19 @@
 #include "../src/symbols.h"
 #include "../thirdparty/ctest.h"
-
-CTEST(file_check, file_exist)
-{
-    FILE* result = fopen("../input.txt", "r");
-    if (result == NULL) {
-        result = 0;
-    }
-    int expected = 0;
-    ASSERT_EQUAL(expected, result);
-}
-CTEST(file_check, file_dosnt_exist)
-{
-    FILE* result = fopen("input.txt", "r");
-    if (result == NULL) {
-        result = 0;
-    }
-    int expected = 0;
-    ASSERT_EQUAL(expected, result);
-}
+#include <stdio.h>
 
 CTEST(check, check_1)
 {
-    char expected[] = "{([])}";
+    char expected[] = "{()}";
     char result[] = "OK";
 
-    ASSERT_STR(expected, result);
+    ASSERT_EQUAL(expected, result);
 }
+
 CTEST(check, check_2)
 {
-    char expected[] = "{[()";
+    char expected[] = "{([)}";
     char result[] = "FAIL";
 
-    ASSERT_STR(expected, result);
+    ASSERT_EQUAL(expected, result);
 }
